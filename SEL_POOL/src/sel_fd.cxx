466fe54b0d962400ad23ac3fa0b2019ea1159220
gps <gps67@github.com>
2023-09-25 16:44:26 +0100

    man page script

--- SEL_POOL/src/sel_fd.cxx
+++ SEL_POOL/src/sel_fd.cxx
@@ -3,32 +3,32 @@
 
 using namespace BASE::SEL_POOL;
 
-sel_fd::
-sel_fd()
-{
-}
+ sel_fd::
+ sel_fd()
+ {
+ }
 
-sel_fd::
-~sel_fd()
-{
-}
+ sel_fd::
+ ~sel_fd()
+ {
+ }
 
  bool
-sel_fd::
+ sel_fd::
  sel_says_read()
  {
  	return INFO("HERE");
  }
 
  bool
-sel_fd::
+ sel_fd::
  sel_says_write()
  {
  	return INFO("HERE");
  }
 
  bool
-sel_fd::
+ sel_fd::
  sel_says_except()
  {
  	return INFO("HERE");
@@ -36,7 +36,6 @@ sel_fd::
 
 
  static sel_pool_base sel_pool_global;
-// ========================
 
  sel_pool_base &
  sel_fd::
