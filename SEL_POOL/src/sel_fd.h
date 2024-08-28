aa06231fda81fb68d2d8b2db4edb51279a0dffaa
gps <gps67@github.com>
2024-08-28 12:43:34 +0100

    sync

--- SEL_POOL/src/sel_fd.h
+++ SEL_POOL/src/sel_fd.h
@@ -1,6 +1,24 @@
 #ifndef sel_fd_H
 #define sel_fd_H
 
+#if 0
+	COMMENT
+
+	This is a rework of fd_selectable for select_says 
+	That was glib based, which is good
+	We also need the event loop from Tcl Python EV Node
+	lib_event Apache LIBC_SELECT POLL_ETC
+
+	To make it easy to stitch this back, it is based on fd_hold
+	That does almost nothing other than call close when refcount zero
+	That makes THIS sel_fd_one the BASE_CLASS of fd
+
+	We are avoiding all MEANINGS from libs_apps, for ZERO use of VOCAB
+	We are invoking the EXISTING basic storage items
+	We are subclassing the EVENT_LOOP_ITEMS for behaviour
+
+#endif
+
 #include "sel_pool_stubs.h"
 #include "obj_ref.h"
 #include "fd_hold.h"
