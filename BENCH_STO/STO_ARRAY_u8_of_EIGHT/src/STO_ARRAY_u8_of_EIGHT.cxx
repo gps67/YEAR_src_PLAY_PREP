e0a989e9dcc03cbbc8e90e0aae3c7c2446c4f5bb
gps <gps67@github.com>
2024-10-18 13:49:29 +0100

    sync

--- BENCH_STO/STO_ARRAY_u8_of_EIGHT/src/STO_ARRAY_u8_of_EIGHT.cxx
+++ BENCH_STO/STO_ARRAY_u8_of_EIGHT/src/STO_ARRAY_u8_of_EIGHT.cxx
@@ -3,10 +3,55 @@
 
 //	#include "dgb.h"
 
+
+ typedef STO::EIGHT_t EIGHT_t; // sort of single IDENT import
+
  extern
 	bool bool_main_test1( int argc, char ** argv ) { // envp
 
+		STO::ARRAY_u8_of_ITEM * PAGE = new STO::ARRAY_u8_of_ITEM();
+
+	// AVAR // CONST_AVAR // need PERMIT to modify
+	// AVAR // CONST_AVAR // need PROFILE to modify and save
+
+		EIGHT_t _one; // 1	// ie support one_is_1
+		EIGHT_t _two; // 0	// ie support large model "_two"
+		EIGHT_t _M1; // -1	// M1 is -1
+		EIGHT_t _M2; // -2	// M2 is -2
+
+		// [M4 [M3 [M2 [M1 [Z0 [1  [2  [3  [0
+		// [M4 [M3 [M2 [M1 [Z0 [1  [2  [3  -- // N_lim
+		// [i3 [M3 [i2 [M1 [Z0 [1  [i2 [3  -- [4==u2==i3_NEXT==i3_N
+		// i3_IDX; [M4[00 [00[u2 // i3_to_u2 // u2_from_i3
+
+		delete PAGE;
 		return FAIL("TODO");
 
 	}
 
+// ==== // END_PRE_START // START_MAIN //
+
+ namespace STO {
+
+	// SPEED says INLINE - particularly when it ASM_GETTER
+	// FLEXI says CALLED - linker gets here, poss via VTBL
+	//
+	EIGHT_t & ARRAY_u8_of_EIGHT :: operator[]( size_t idx ) {
+		return * ARRAY[idx];
+	}
+
+	ARRAY_u8_of_EIGHT:: ARRAY_u8_of_EIGHT() : N(0) {
+	// ARRAY_u8_of_EIGHT() : N(0), ARRAY(NULL) 
+	 	ARRAY = (PAGE_u8_of_EIGHT_t *) MMAP_alloc( sizeof(*ARRAY) ); // I always MEMSET_ZERO
+		// sometime I HOPE ZERO and attempt to avoid DOUBLE_MEMSET //
+		// the debugged version is MEMSET_ZERO // GUARANTEED //
+	//	if(!ARRAY) return FAIL_FAILED(); // use NULL properly 
+	//	if(!ARRAY) return FAIL_throw(); // retain NULL properly 
+	//	if(!ARRAY)        FAIL_throw(); // retain NULL properly 
+	//	if(!ARRAY) throw THROW_dgb_fail("NULL from MALLOC"); // GEN this as default
+		if(!ARRAY)       THROW_dgb_fail("NULL from MALLOC"); // GEN this as default
+		// errno and errm is already correctly pre_set
+		// we just need to walk back the THROW or FAIL up the stack
+		 u8_idx = 0 ;
+	 }
+ }; // STO
--- BENCH_STO/STO_ARRAY_u8_of_EIGHT/src/STO_ARRAY_u8_of_EIGHT.h
+++ BENCH_STO/STO_ARRAY_u8_of_EIGHT/src/STO_ARRAY_u8_of_EIGHT.h
@@ -12,7 +12,64 @@ namespace STO {
 	/*
 		STO ARRAY_u8_of_EIGHT .h
 
+		STO is itself GROW_able
+
+		It uses HEAPS of size u7_u15_u31_u63 INT_PLUS
+		It uses HEAPS of size i8_i16_i32_i64 INT_MINUS
+		It uses HEAPS of size i4_i12_i24_i48 INT_BITFIELDS //
+		// _PLUS_or_MINUS
+
+		STO uses L_M_z_N_R 	// A_LOT :-)
+		STO uses HEAP_ITEM SPEC_INST CALL_SPEC as API
+		STO uses HEAP LISTS TABLES HASH INDEX_IDX INDEX_STR
+		HEAP uses ... i8_i8 i4_i12 i8_i24
+		HEAP uses ...
+
+			i32_i32		i32_i32_WORD_PAIR
+
+			i8_i8		OPCODE
+			i4_i12		i4_HEAP i12_ITEM
+			i8_i24		i8_HEAP i24_ITEM
+		
+			There are only a few MAIN tables,
+			CODE_POINT of DECL
+			which fits in easily with i4_i12
+			Alloc an item whenever you need one,
+			and the total is in the hundreds,
+			just outside BYTE, but below 4K
+			(and expandable to u128 but mostly u64 or u32)
+
+			i128
+			-GROW-STEP-
+			u64
+			i64
+			u63
+			-GROW-STEP-
+				i48
+			-GROW-STEP-
+			u32
+			i32
+			u31
+			-GROW-STEP-
+				i24
+			-GROW-STEP-
+			u16
+			i16
+			u15
+			-GROW-STEP-
+				i12
+			-GROW-STEP-
+			u8
+			i8
+			u7
+
+
+
 		TEMPLATE for MULT_1248
+
+		TODO: M_z_N for L_R as P0P2 as V0V2
+
+
 	*/
 
 // CPU32
@@ -20,6 +77,25 @@ namespace STO {
 
 // typedef u32 WORD_t; // CPU specific
    typedef u64 WORD_t; // MEM align EIGHT_t or FOUR_t // u32_u32 //
+  
+// GROW // u7_u15_u23_u31 _u63
+// GROW // i8_i16_i24_i32 _i64
+// GROW // u8_u16_u24_u32 _u64
+// GROW // i4_i12 // u3 u11 // i4_MINUS
+// GROW // i8_i24 // u7 u23 // i8_MINUS
+
+// L_R //
+// M_N //
+// _Z_ // 
+// ALLOC_MINUS
+// ALLOC_PLUS
+
+	// u9 N; //
+   typedef u16 u9; 	// is_wider // BEWARE when N needs 17'th bit
+   typedef u9  u9_or_i10_or_wider; //
+   typedef u9 u8_then_1; // u9 or i10 or wider
+   typedef u8 i8_then_u8; // u9 or i10 or wider needed for N_FULL // 256-1
+   typedef u8 u8_idx_t; // LIBR can force outer N_PAGES_of_256 PAGE_of_256
 
 
  struct LOCK_t {
@@ -42,12 +118,6 @@ typedef	EIGHT_t   PAGE_u8_of_EIGHT_t[ 256 ];	// ONE_PAGE
 
 	PAGE_u8_of_EIGHT_t * ARRAY;
 
-	// u9 N; //
-	typedef u16 u9; 	// is_wider // BEWARE when N needs 17'th bit
-	typedef u9  u9_or_i10_or_wider; //
-	typedef u9 u8_then_1; // u9 or i10 or wider
-	typedef u8 i8_then_u8; // u9 or i10 or wider needed for N_FULL // 256-1
-	typedef u8 u8_idx_t; // LIBR can force outer N_PAGES_of_256 PAGE_of_256
 	// u9_N_t // 255 + 1 //
 	u8_then_1 N;
 
@@ -57,24 +127,10 @@ typedef	EIGHT_t   PAGE_u8_of_EIGHT_t[ 256 ];	// ONE_PAGE
 	 // every ARRAY has a single builtin TEMP CSR // obtain LOCK get CSR
 	 // u8_idx is that CSR
 
-	 ARRAY_u8_of_EIGHT() : N(0) {
-	// ARRAY_u8_of_EIGHT() : N(0), ARRAY(NULL) 
+	 ARRAY_u8_of_EIGHT();
 
-	 	ARRAY = (PAGE_u8_of_EIGHT_t *) MMAP_alloc( sizeof(*ARRAY) ); // I always MEMSET_ZERO
-		// sometime I HOPE ZERO and attempt to avoid DOUBLE_MEMSET //
-		// the debugged version is MEMSET_ZERO // GUARANTEED //
-
-	//	if(!ARRAY) return FAIL_FAILED(); // use NULL properly 
-	//	if(!ARRAY) return FAIL_throw(); // retain NULL properly 
-	//	if(!ARRAY)        FAIL_throw(); // retain NULL properly 
-	//	if(!ARRAY) throw THROW_dgb_fail("NULL from MALLOC"); // GEN this as default
-		if(!ARRAY)       THROW_dgb_fail("NULL from MALLOC"); // GEN this as default
-		// errno and errm is already correctly pre_set
-		// we just need to walk back the THROW or FAIL up the stack
-
-		 u8_idx = 0 ;
-	 }
-  };
+	EIGHT_t & operator[]( size_t idx );
+ };
 
  struct ARRAY_u8_of_ITEM : public ARRAY_u8_of_EIGHT {
 	// we have only implemented EIGHT
