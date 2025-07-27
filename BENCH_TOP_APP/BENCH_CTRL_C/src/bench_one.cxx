3529f0cf656ae4edf2dd8d56902c719745dc2fea
gps <gps67@github.com>
2025-07-27 02:49:43 +0100

    so tired

--- BENCH_TOP_APP/BENCH_CTRL_C/src/bench_one.cxx
+++ BENCH_TOP_APP/BENCH_CTRL_C/src/bench_one.cxx
@@ -10,26 +10,65 @@
 
 using namespace APPS;
 
-//! tv_rem
+//! linux calls __START_RTFM_ libc calls main()
 int main(int argc, char ** argv )
 {
 	// holder does this // setlocale(LC_ALL,"");
+	// run the init functions in an incomplete environment
+	// decode_argv() would recognise # COMMAND FILENAME //
 	char * filename = NULL;
 	if( argc == 2 ) {
 		filename = argv[1];
 	}
+	// extra { braces } ensure local vars get CTOR done within function
 	{
+		// This does NOT change with each new applet, holds base_class
+		// see LIBR for base_class options
+		// your derived UDEF is bench_top_app 
 		TOPAPP_Holder_gtk holder(&argc, &argv);
 //		new BENCH::bench_top_app( &holder, filename );
+
+		// based on a LIBR_base_class_for_any_app
+		// you derive bench_top
+		// the CTOR also installs SELF into HOLDER
+		// TODO setup on_EXIT() to tell holder and DTOR happens
+		// TODO delay DTORS into a HIGH_NEED_DTOR and SKIP_DTOR
+		// filename would be argv_decoder or TASK scedule or SCRIPT
 		new bench_top_app( &holder, filename );
+		// bench_top CTOR installed a lot of CODE vectors
+		// bench_top C++ class provides "app" as a class += code
+		// you write bench_one_app( &holder, TODO_ARGV )
+
+		// the main REPL loop is gx_main and installed onEvent vectors
 		holder.call_gx_main_and_exit();
+		// but exit means DTOR might never get called
+		// so we adapt, knowing DTOR is called after exit() - ie not
 	}
 	ref_tally.print_leaked_report();
 	return 0;
 }
 
-// DOXYGEN Main Page
+
 
 /*!
+	bench_top_app is to be replaced with UDEF
+	bench_top_app already is a GUI WINDOW with a MENU main PANEL and MORE
+	bench_top_app is an APPLET or APP running within DESKTOP BENCH
+	bench is currently DESKTOP # future BENCH as BENCH for TASK 
+	TOP_APP is kept alive by main_window and some tasks
+	TOP_APP can kill those tasks, if you are sure about that
+	TOP_APP is a future APPLET # DESKTOP is shared GUI space
+	GUI_TOP_APP is UDEF_BASE
+
+	GUI_TREE_WALK is mc starter
+
+
+	Initially each APP has it's own MAIN_WINDOW
+	Already each APP has XCX own FILE or TASK
+	Ticket becomes a TASK becomes an active APPLET
+
+
+
+
 
 */
--- PSG_SCRIPT/GEN_YACC_eg/GEN_YACC_bison_info/src/gen_yacc.cxx
+++ PSG_SCRIPT/GEN_YACC_eg/GEN_YACC_bison_info/src/gen_yacc.cxx
@@ -43,6 +43,12 @@ bool lex_yaxx_UDEF_ONE_t:: build_tree_yacc()
 // TODO // PSG SUB_LEX // 
 
 bool parse_ARGV_bool_main( int argc, char ** argv ) {
+	//
+	// Parse_ARGV
+	// call BOOL_MAIL // CT_RT has VALUE= PICK MACRO from LIST and WRAP CODE
+	//
+	// PID EXIT is errno // via_FILTER // ZERO_means_PASS _DONE //
+	//
 	lex_yaxx_UDEF_ONE_t UDEF_ONE;
 	if(! UDEF_ONE.build_tree() ) return FAIL_FAILED();
 
@@ -52,16 +58,103 @@ bool parse_ARGV_bool_main( int argc, char ** argv ) {
 }
 
 bool bool_main( int argc, char ** argv ) {
+	// 
+	// sweep_environ_once(); // called from here // loaded to table
+	// or called from main()
+	// 
 	/*
-		every mini app provides %s_bool_main(argc, argv
+		every mini app provides %s_bool_main(argc, argv) // envp 
+		//  extern char **environ; // NULL terminated array of STR0
+		// STR0 == NULL // end_of_list_marker // OPTION ABSENT NULL
+		// STR0 == "NAME=VALUE" // 
+		// so there is no reason to require enp from main
+		// OPTION set environ from PRE_LOAD or FILTER
+		// avoid use in code other than as glibc feature
+		// we sweep environ once 
+
+		TODO ADD envp and C_STACK has introspection
+
+		SESS has what Tcl would call ClientData a 64_bit LOHI WORD
+		LOHI is required 
+		EXPR is ALWAYS as the i48 needs to be extraced from the i64
+		EXPR is i8_i8_OPCODE i16_Payload i32_Payload i64_WORD u64_WORD
+		ADDR is when i48 is i64 is PTR # ALIAS PTR EA ADDR AREF IDX
+		EXPR is i8_i8_OPCODE i16_zero i16_one i16_two
+
+		 i8_i8 OPCODE is enough to perform entire CPU TRANSACTION BATCH
+
+			 i8_i8 OPCODE 
+		EXPR is
+			i8_i8_OPCODE
+			i16_Payload
+			i32_Payload
+		or
+			i64_WORD
+		OPTION or -see LIBR_SIGN // AUTO $next_line is ENTIRE WORD
+
+			// LIBR_CPU uses u64 a LOT
+			// BITFIELD u16_i16_i16_i16 // SIGNED under UNSIGNED
+			// BITFIELD usually uses 2 shifts second SIGNED or NOT
+			// BITFIELD_TOP only needs 1 shift SIGNED obvs
+			// BITFIELD ENTIRE needs 0 shifts CPU provides SIGN
+			// BITFIELD ENTIRE LIBR.SIGN 
+			// BITFIELD ENTIRE LIBR.SIGNED // BYTE_FIELD ENTIRE
+			// BITFIELD ENTIRE LIBR.UNSIGNED // ENTIRE UNS
+			// BITFIELD CPU_MODE _HALF
+			// _HALF _or_PARTIAL or_BITFIELD or_COMPLETE
+			//
+			// HALF_HALF
+			// HALF_HIGH
+			// 
+			// HIGH_HALF // BIT order in DIAG // LOHI in MEM
+			// i32_i32 provides 
+			// 
+			// We wrote LIBR with 2 mindsets
+			// CPU_32 can load HALF_HIGH
+			needs 0 shifts CPU provides SIGN
+			//
+			u64_WORD
 	*/
 	return parse_ARGV_bool_main( argc, argv );
 }
 
+// USAGE STYLE { before EOLN AUTO catches { HERE } } // not next line
+// USAGE STYLE { AFTER BOLN AUTO catches { HERE } } // needs next line AVAIL
+
+bool sweep_environ_one ( int argc, char ** argv ) {
+	return PASS("copy environ to MMAP area register EA_NAME in TABLE");;
+	return TRUE;
+	return INFO("TEMPLATE of APP_MAIN # it creates TOKEN of SPELLING");;
+	return INFO("ROM reuses SPELLING as P0P2 or STR0");
+	return INFO("SPELLING as P0P2 from STR0");
+	return INFO("PARSABLE SPELLING as P0P2 from P0"); // cident97 
+	return INFO("see libs_apps TOPAPP_Task.h"); // TODO
+
+	// C_ident_97 // _97 is SESS_says_PICK_97_for_99 // 99 is iso_99
+	// vfs_ident_97 // vfs is users prefs adds +-= adds SP _or_NOT
+	// CXX_ident_97 // CXX can start with DIGIT and AUTO rename
+	// CXX_intro ALWAYS_adds_DUNDER_prefix // so a1 = a2
+	// SUB_LEX ALWAYS_adds_LHS // so a1 = a2 // of whatever _97 says
+	return TRUE;
+	return FAIL("unwritten");;
+}
+
+
 int main( int argc, char ** argv ) {
-	if( bool_main( argc, argv ) ) {
-		return 0;
+	if( !sweep_environ_one(argc,argv) ) {
+		goto failed; // perfectly acceptable in DIALECT
 	}
+	if( bool_main( argc, argv ) ) {
+		return 0; // nothing more to say // DONE // OK //
+	}
+
+	// C++ can goto failed // very easy just call END_SCOPE DTOR
+	// C++ can endscope // all in funcs anyway // nothing to do
+	// LIBR should if(!errno) then search for APP LIBR API error
+	// LIBR should if(!errno) then set it to 22 then ask LIBR
+	// UDEF this is what libs_apps TOPAPP_Task.h should wrap
+
+ failed:
 	FAIL_FAILED();
 	INFO("errno %d", (int) errno );
 	if(errno) return errno;
